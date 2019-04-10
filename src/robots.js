04d98f6a4849147a6f994c296720fe4d04866d0e
IDD2009 <dan99ionascu@yahoo.com>
2019-04-10 17:13:37 +0300

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'Shanna@melissa.tv'
+	},
+	{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'Nathan@somenthing.com'
+	},
+	{
+		id: 4,
+		name: 'Jhon Doe',
+		username: 'Jazzy',
+		email: 'JDooe@email.com'
+	},
+	{
+		id: 5,
+		name: 'Crazy Frog',
+		username: 'Jumpy',
+		email: 'froggg@gemail.com'
+	},
+	{
+		id: 6,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 7,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'Shanna@melissa.tv'
+	},
+	{
+		id: 8,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'Nathan@somenthing.com'
+	},
+	{
+		id: 9,
+		name: 'Jhon Doe',
+		username: 'Jazzy',
+		email: 'JDooe@email.com'
+	},
+	{
+		id: 10,
+		name: 'Crazy Frog',
+		username: 'Jumpy',
+		email: 'froggg@gemail.com'
+	}
+];
