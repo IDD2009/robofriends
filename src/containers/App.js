6a0049a4dda8d1a4b093a2fc6de89b5a7eb07809
IDD2009 <dan99ionascu@yahoo.com>
2019-04-23 22:31:40 +0300

    final modifications

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,45 +4,38 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
 	}
 }
 
 class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots: []
-		}
-	}
-
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => this.setState({robots: users}));
+		this.props.onRequestRobots();
 	}
 
 
 	render(){
-		// const { robots } = this.state;
-		const { searchField, onSearchChange } = this.props;
-		const filteredRobots = this.state.robots.filter(robots => {
+		const { searchField, onSearchChange, robots, isPending } = this.props;
+		const filteredRobots = robots.filter(robots => {
 			return robots.name.toLowerCase().includes(searchField.toLowerCase());
 		})
-		if (this.state.robots.lenght === 0){
-			return <h1>Loading</h1>
-		}	else {
-			return (
+		return  isPending ?
+			<h1>Loading</h1> :
+		(
 				<div className='tc'>
 					<h1>RoboFriends</h1>
 					<SearchBox searchChange={onSearchChange}/>
@@ -50,8 +43,8 @@ class App extends Component {
             <CardList robots={filteredRobots} />
           </Scroll>
 				</div>
-			);
-		}
+		);
+		
 	}
 }
 
