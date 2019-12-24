9bdb6c715d73b4e4b910b2d0e9dda89a348cdfee
IDD2009 <dan99ionascu@yahoo.com>
2019-12-24 18:41:44 +0200

    removed space

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, email, id}) => {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
 			<img alt='robots' src={`https://robohash.org/${id}?size=200x200&set=set5`} />
-			<div> 
+			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
 			</div>
